c455b9b600ade0b149f2c7cf9065ec2e7f8a4016
Abdullah <tshadej3@gmail.com>
2024-04-30 13:35:33 -0400

    Created the Triangle and vertex class and added the code to draw the skeleton of 1 triangle.

--- src/driver.java
+++ src/driver.java
@@ -1,5 +1,9 @@
 import javax.swing.*;
-import java.awt.*; 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.awt.Color;
+import java.awt.geom.Path2D;
 
 public class driver {
 
@@ -25,8 +29,31 @@ public class driver {
     		   Graphics2D g2 = (Graphics2D) g; 
     		   g2.setColor(Color.BLACK);
     		   g2.fillRect(0, 0, getWidth(), getHeight());
+    		   
+    		   g2.translate(getWidth() / 2, getHeight() / 2);
+    		   g2.setColor(Color.WHITE);
+    		   
+    		   // this will contain a list of triangle of triangle objects 
+    		   List<Triangle> tris = new ArrayList<>();
+    		   tris.add(new Triangle(new Vertex(100, 100, 100), new Vertex(-100, -100, 100), new Vertex(-100, 100, -100), Color.WHITE));
+    		   
+    	       
+    	       
+    		   for (Triangle t : tris) {
+    			    Path2D path = new Path2D.Double();
+    			    path.moveTo(t.v1.x, t.v1.y); // starts the path here
+    			    path.lineTo(t.v2.x, t.v2.y); // draws a line from first vertex to the next one. 
+    			    path.lineTo(t.v3.x, t.v3.y);
+    			    path.closePath();
+    			    g2.draw(path);
+    			}
+    		   
+    		   
     	   }
        };
+       
+       
+     
        
        pane.add(renderPanel, BorderLayout.CENTER);
        
